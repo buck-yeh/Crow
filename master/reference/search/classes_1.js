2e149da89e705fac14611cbe7c94cd941f2a12de
buck-yeh <buck-yeh@users.noreply.github.com>
2024-05-12 05:49:19 +0000

    Deploying to gh-pages from @ buck-yeh/Crow@1b6a4f4301e455862c25b5dc9051c9cad0ff826c 🚀

--- master/reference/search/classes_1.js
+++ master/reference/search/classes_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['baserule_226',['BaseRule',['../classcrow_1_1_base_rule.html',1,'crow']]],
-  ['blueprint_227',['Blueprint',['../classcrow_1_1_blueprint.html',1,'crow']]]
+  ['baserule_244',['BaseRule',['../classcrow_1_1_base_rule.html',1,'crow']]],
+  ['blueprint_245',['Blueprint',['../classcrow_1_1_blueprint.html',1,'crow']]]
 ];
